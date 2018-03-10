818dcd8aaa7bbbec6495c29568a1b984052d3a47
Saulo de Carvalho Neto <saulocn@gmail.com>
2018-03-10 11:43:04 -0300

    - Organizando o Projeto e realizando a primeira busca na API; - Implementação de um módulo pra lidar com a resposta da Promise;

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,9 @@
+import { handleStatus } from './utils/promise-helpers.js';
+
 document
 .querySelector('#myButton')
-.onclick = () => {
-    alert("Oi!");
-}
+.onclick = () =>
+    fetch('http://localhost:3000/notas')
+    .then(handleStatus)
+    .then(notas=> console.log(notas))
+    .catch(console.log);
