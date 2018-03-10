d8d07f3dabb7e2f145f4ea21c37f6011f3708f76
Saulo de Carvalho Neto <saulocn@gmail.com>
2018-03-10 12:07:00 -0300

    - Implementando o flatMap para o Array; - Organizando melhor o código; - Implementando um handle para o log;

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,25 @@
-import { handleStatus } from './utils/promise-helpers.js';
+import { handleStatus, log } from './utils/promise-helpers.js';
+import './utils/array-helpers.js';
 
 document
 .querySelector('#myButton')
 .onclick = () =>
     fetch('http://localhost:3000/notas')
     .then(handleStatus)
-    .then(notas=> console.log(notas))
+    //.then(notas => notas.map(nota=> nota.itens))
+    //.then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
+    .then(notas=> notas
+        .$flatMap(nota=> nota.itens)
+        .filter(item => item.codigo == '2143')
+        .reduce((total, item) => total+item.valor, 0)
+    )
+    /*.then(itens=> {
+        console.log(itens);
+        return itens;
+    })
+    .then(log)
+    .then(itens => itens.filter(item => item.codigo == '2143'))
+    .then(log)
+    .then(itens => itens.reduce((total, item) => total+item.valor, 0))*/
+    .then(console.log)
     .catch(console.log);
