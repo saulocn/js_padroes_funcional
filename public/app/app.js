7ca2acc005a18ede4ad880d20cafd163642701f9
Saulo de Carvalho Neto <saulocn@gmail.com>
2018-03-10 12:21:58 -0300

    - Separando responsabilidades e adequando os dados recebidos;

--- public/app/app.js
+++ public/app/app.js
@@ -1,25 +1,18 @@
-import { handleStatus, log } from './utils/promise-helpers.js';
+import { log } from './utils/promise-helpers.js';
+import { notasService as service } from './nota/service.js';
 import './utils/array-helpers.js';
+
+
+
+
 
 document
 .querySelector('#myButton')
 .onclick = () =>
-    fetch('http://localhost:3000/notas')
+    /*fetch('http://localhost:3000/notas')
     .then(handleStatus)
-    //.then(notas => notas.map(nota=> nota.itens))
-    //.then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
-    .then(notas=> notas
-        .$flatMap(nota=> nota.itens)
-        .filter(item => item.codigo == '2143')
-        .reduce((total, item) => total+item.valor, 0)
-    )
-    /*.then(itens=> {
-        console.log(itens);
-        return itens;
-    })
-    .then(log)
-    .then(itens => itens.filter(item => item.codigo == '2143'))
-    .then(log)
-    .then(itens => itens.reduce((total, item) => total+item.valor, 0))*/
+    service.listAll()
+    .then(sumItems('2143'))*/
+    service.sumItems('2143')
     .then(console.log)
     .catch(console.log);
