9ce41094a22fef87883fb565dfa2392c86a24dd8
Saulo de Carvalho Neto <saulocn@gmail.com>
2018-03-30 21:55:32 -0300

    - Limitando a 3 as chamadas a ação do usuário; -  Não permitindo que o usuário clique várias vezes no botão limitando excessos de cliques a 1 clique;

--- public/app/app.js
+++ public/app/app.js
@@ -1,20 +1,42 @@
 import { log } from './utils/promise-helpers.js';
 import { notasService as service } from './nota/service.js';
 import './utils/array-helpers.js';
+import {takeUntil, debounceTime} from './utils/operators.js';
+
+const action = debounceTime(500, 
+                    takeUntil(3, () => 
+                        service.sumItems('2143')
+                        .then(console.log)
+                        .catch(console.log)
+                    )
+                );
 
 /*
-const ehDivisivel = (divisor, numero) => !(numero%divisor);
-const ehDivisivelPorDois = ehDivisivel.bind(null, 2);
+const operation1 = takeUntil(3, () => 
+                    service.sumItems('2143')
+                    .then(console.log)
+                    .catch(console.log)
+                );
 
-console.log(ehDivisivelPorDois(10));
-console.log(ehDivisivel(5, 2));
-console.log(ehDivisivelPorDois(2, 12));
+const operation2 = debounceTime(500, operation1);
+
+
+
+const showMessage = () => console.log('Opa!');
+const operation2 = debounceTime(500, showMessage);
+operation2();
+operation2();
+operation2();
+
+const showMessage = () => console.log("oi");
+const operation = takeUntil(3, showMessage);
+
+
+let counter = 10;
+
+while(counter--) operation();
 */
-
 
 document
 .querySelector('#myButton')
-.onclick = () =>
-    service.sumItems('2143')
-    .then(console.log)
-    .catch(console.log);
+.onclick = action;
