b91a3105f59675fd8ce78ca0ebf75eba7ffb663c
Saulo de Carvalho Neto <saulocn@gmail.com>
2018-03-31 08:04:46 -0300

    - Utilização de pipe para melhorar o código na execução das funções: - takeUntil - debounceTime;

--- public/app/app.js
+++ public/app/app.js
@@ -1,41 +1,21 @@
 import { log } from './utils/promise-helpers.js';
 import { notasService as service } from './nota/service.js';
 import './utils/array-helpers.js';
-import {takeUntil, debounceTime} from './utils/operators.js';
+import {takeUntil, debounceTime, partialize, pipe} from './utils/operators.js';
 
-const action = debounceTime(500, 
-                    takeUntil(3, () => 
-                        service.sumItems('2143')
-                        .then(console.log)
-                        .catch(console.log)
-                    )
-                );
 
-/*
-const operation1 = takeUntil(3, () => 
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+);
+
+
+const action = operations(() => 
                     service.sumItems('2143')
                     .then(console.log)
                     .catch(console.log)
                 );
-
-const operation2 = debounceTime(500, operation1);
-
-
-
-const showMessage = () => console.log('Opa!');
-const operation2 = debounceTime(500, showMessage);
-operation2();
-operation2();
-operation2();
-
-const showMessage = () => console.log("oi");
-const operation = takeUntil(3, showMessage);
-
-
-let counter = 10;
-
-while(counter--) operation();
-*/
+                
 
 document
 .querySelector('#myButton')
