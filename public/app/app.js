83dfe62f6b3d1382a9d78c82e95574f408aa7b74
Saulo de Carvalho Neto <saulocn@gmail.com>
2018-04-01 15:57:21 -0300

    -

--- public/app/app.js
+++ public/app/app.js
@@ -4,28 +4,6 @@ import './utils/array-helpers.js';
 import {takeUntil, debounceTime, partialize, pipe} from './utils/operators.js';
 import {EventEmitter} from './utils/event-emitter.js';
 
-/*
-const value = Maybe.of(10)
-.map(value => value+10)
-.map(value => value+30)
-.getOrElse(0);
-
-
-
-const textToArray = textM => textM.map(text => Array.from(text));
-const arrayToText = arrayM => arrayM.map(array => array.join(''));
-const transform = pipe(textToArray, arrayToText);
-
-
-//const result = transform('Cangaceiro');
-//const result = transform(null);
-//const result = transform(Maybe.of('Cangaceiro'));
-const result = transform(Maybe.of(null));
-
-alert(result.getOrElse(''));
-//alert(value);
-*/
-
 const operations = pipe(
     partialize(takeUntil, 3),
     partialize(debounceTime, 500)
@@ -43,6 +21,3 @@ document
 .querySelector('#myButton')
 .onclick = action;
 
-
-//EventEmitter.on('itensTotalizados', console.log);
-//EventEmitter.on('itensTotalizados', total => alert(total));
