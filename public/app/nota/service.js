800d45bf91de64f38aa5c096e89f61315ec70414
Saulo de Carvalho Neto <saulocn@gmail.com>
2018-04-01 15:56:37 -0300

    - Implementação de uma mônada para se precaver de um retorno null ou undefined;

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,11 +1,12 @@
 import {handleStatus} from '../utils/promise-helpers.js';
 import {partialize, pipe} from '../utils/operators.js';
+import { Maybe } from '../utils/maybe.js';
 
 const API = 'http://localhost:3000/notas';
 
-const getItemsFromNota = notas => notas.$flatMap(nota=> nota.itens);
-const filterItemsByCode = (code, items) => items.filter(item => item.codigo == code);
-const sumItemsValue = items => items.reduce((total, item) => total+item.valor, 0);
+const getItemsFromNota = notasM => notasM.map(notas => notas.$flatMap(nota=> nota.itens));
+const filterItemsByCode = (code, itemsM) => itemsM.map(items => items.filter(item => item.codigo == code));
+const sumItemsValue = itemsM => itemsM.map(items => items.reduce((total, item) => total+item.valor, 0));
 
 
 
@@ -19,6 +20,7 @@ export const notasService = {
     listAll() {
         return fetch(API)
             .then(handleStatus)
+            .then(notas => Maybe.of(notas))
             .catch(err => {
                 console.log(err);
                 return Promise.reject('Não foi possível obter as notas fiscais.');
@@ -33,13 +35,7 @@ export const notasService = {
             sumItemsValue);
             
         return this.listAll()
-            .then(sumItems);
-
-            /*.then(notas => sumItemsValue(
-                                filterItems(
-                                    getItemsFromNota(notas)
-                                )
-                            )
-                );*/
+            .then(sumItems)
+            .then(result => result.getOrElse(0));
     }
 };
